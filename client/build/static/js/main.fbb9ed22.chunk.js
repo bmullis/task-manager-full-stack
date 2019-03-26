4118f824281a900e08e187c18fef6a0f19f415d7
Brian Mullis <brian@smartrent.com>
2019-03-26 16:38:45 -0700

    Add production build files for the client app

---
+++ client/build/static/js/main.fbb9ed22.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},9:function(e,n,t){"use strict";t.r(n);var r=t(0),o=t.n(r),c=t(2),u=function(){return o.a.createElement("div",null,o.a.createElement("h1",null,"App Goes Here"))};t.n(c).a.render(o.a.createElement(u,null),document.querySelector("#root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.fbb9ed22.chunk.js.map
