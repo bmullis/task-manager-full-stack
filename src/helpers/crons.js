f60740108d3f90c4470a6a6a047c92c1992ee1b0
Brian Mullis <brian@smartrent.com>
2019-04-08 07:42:42 -0700

    Changed email delivery service to SendGrid

--- src/helpers/crons.js
+++ src/helpers/crons.js
@@ -2,8 +2,11 @@ const cron = require('node-cron')
 const User = require('../models/user')
 const Task = require('../models/task')
 const Notification = require('../models/notification')
+const sgMail = require('@sendgrid/mail')
 
-const checkOverdue = () => cron.schedule('50 22 * * *', () => {
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const checkOverdue = () => cron.schedule('00 15 * * *', () => {
   User.find({}).then((users) => {
     users.forEach(async function(user) {
       const tasks = await Task.find({ created_by: user._id })
@@ -21,8 +24,15 @@ const checkOverdue = () => cron.schedule('50 22 * * *', () => {
           message: `You have ${overdueAmount} overdue tasks. You can reschedule these tasks, or get them finished!`,
           to_user: user._id
         })
-    
         notification.save()
+
+        const msg = {
+          to: user.email,
+          from: 'taskapp@taskapp.io',
+          subject: `${overdueAmount} Overdue Tasks`,
+          text: `${user.firstName}, you have ${overdueAmount} over tasks! Make sure you get them done.`
+        }
+        sgMail.send(msg)
       }
     })
   })
